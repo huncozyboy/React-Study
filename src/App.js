36662e0ebba01ca9b1abc5a17415e10414feff2a
huncozyboy <leejh5838@naver.com>
2025-04-20 23:51:10 +0900

    refactor : state 적용

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="word wide web"></Subject>
+          <Subject 
+            title={this.state.subject.title} 
+            sub={this.state.subject.sub}>
+          </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" sub="HTML is HyperText Markup Language."></Content>
       </div>
     );
