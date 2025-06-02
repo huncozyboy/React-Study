2ba2ecaa54425634814710d0f023d10d67c00dca
huncozyboy <leejh5838@naver.com>
2025-06-02 17:43:46 +0900

    feat : useReducer 문법 연습

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -3,6 +3,7 @@ import { useState , useRef} from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
+import Exam from './components/Exam';
 
 const mockData = [
     {
