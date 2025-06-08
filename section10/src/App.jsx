7504fe67e2a7971fe7a510203005263f5114cfc9
huncozyboy <leejh5838@naver.com>
2025-06-08 12:00:00 +0900

    개행 추가

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -29,16 +29,20 @@ const mockData = [
   function reducer(state, action) {
     switch (action.type) {
       case 'CREATE':
+
         return [action.data, ...state];
       case 'UPDATE':
+
         return state.map((item)=> 
           item.id === action.targetId
             ? {...item, isDone: !item.isDone} 
             : item
         );
       case 'DELETE':
+
         return state.filter((item) => item.id !== action.targetId);
       default:
+        
         return state;
     }
   }
