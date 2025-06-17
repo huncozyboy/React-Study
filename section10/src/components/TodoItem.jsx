4596dcb65271fe70b7fb72c00f16ea3fbe716626
huncozyboy <leejh5838@naver.com>
2025-06-18 00:32:19 +0900

    생성, 수정, 삭제 메서드 리렌더링시 재생성 방지

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -34,22 +34,24 @@ const TodoItem = ({
     );
 };
 
-export default memo(TodoItem, (prevProps, nextProps) => {
-    // 반환값에 따라, props가 변경되지 않은 것으로 판단
-    // true: 변경되지 않은 것으로 판단
-    // false: 변경된 것으로 판단
-    if (prevProps.isDone !== nextProps.isDone) {
-        return false; // isDone이 변경되었으므로, 렌더링
-    }
-    if (prevProps.content !== nextProps.content) {
-        return false; // content가 변경되었으므로, 렌더링
-    }
-    if (prevProps.date !== nextProps.date) {
-        return false; // date가 변경되었으므로, 렌더링
-    }
-    if (prevProps.id !== nextProps.id) {
-        return false; // id가 변경되었으므로, 렌더링
-    }
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//     // 반환값에 따라, props가 변경되지 않은 것으로 판단
+//     // true: 변경되지 않은 것으로 판단
+//     // false: 변경된 것으로 판단
+//     if (prevProps.isDone !== nextProps.isDone) {
+//         return false; // isDone이 변경되었으므로, 렌더링
+//     }
+//     if (prevProps.content !== nextProps.content) {
+//         return false; // content가 변경되었으므로, 렌더링
+//     }
+//     if (prevProps.date !== nextProps.date) {
+//         return false; // date가 변경되었으므로, 렌더링
+//     }
+//     if (prevProps.id !== nextProps.id) {
+//         return false; // id가 변경되었으므로, 렌더링
+//     }
     
-    return true; // 모든 props가 동일하므로, 렌더링하지 않음
-});
+//     return true; // 모든 props가 동일하므로, 렌더링하지 않음
+// });
+
+export default memo(TodoItem);
